d610ef21ace485c867278c1e208464cdc1de2a51
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-01 12:28:42 +0000

    comment-good-finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'home/about'=>'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+
     resources :book_comments, only: [:create, :destroy]
+
   end
 
 end
