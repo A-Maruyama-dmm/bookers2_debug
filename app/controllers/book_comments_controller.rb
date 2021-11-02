6672ffbb61567fe403457136dbaa1dad38c58e63
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-02 06:53:15 +0000

    comment-good-finishadd

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,14 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)   # =comment = PostComment.new(post_comment_params)comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)   # =comment = PostComment.new(post_comment_params)comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    if @book_comment.save
     redirect_to book_path(book)
-
+    else
+    render 'books/show'
+    end
   end
 
   def destroy
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -3,4 +3,7 @@ class BookComment < ApplicationRecord
   belongs_to :book
 
 
+  validates :comment, presence: true
+
+
 end
