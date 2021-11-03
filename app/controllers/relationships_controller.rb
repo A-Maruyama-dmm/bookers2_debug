ded4f82999321460736fcaed25106272a7636126
A-Maruyama-dmm <com.mac.dm@gmail.com>
2021-11-03 06:47:22 +0000

    followeradd

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,16 +10,6 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
-#————————フォロー・フォロワー一覧を表示する-————————————
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
-
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
-  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
   root 'homes#top'
 
   get 'home/about'=>'homes#about'
+
   resources :users,only: [:show,:index,:edit,:update] do
-
+    member do   #resourcesは7つのルーティングを行ってくれますが、memberを使うことで、別のルーティングを追加できます。
+    get :follows, :followers
+  end
   resource :relationships, only: [:create, :destroy]
-  get 'followings' => 'relationships#followings', as: 'followings'
-  get 'followers' => 'relationships#followers', as: 'followers'
-
+  #resource :relationships, only: [:create, :destroy]
+  #get 'followings' => 'relationships#followings', as: 'followings'
+  #get 'followers' => 'relationships#followers', as: 'followers'
   end
 
   resources :books, only: [:new, :create, :index, :show, :destroy] do
